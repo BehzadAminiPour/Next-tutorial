87430c7617cb1cbf4a89a27b4dab32d7a97cb5b5
Behzad Aminipour <behzadaminipour87@gmail.com>
2024-01-10 13:21:49 +0330

    updated

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,9 +1,12 @@
 import Link from "next/link";
+import Image from "next/image";
+import Logo from "./dojo-logo.png";
 
 export default function Navbar() {
   return (
     <nav>
-      <h1>Hello Bezi</h1>
+      <Image src={Logo} alt="logo" width={70} placeholder="blur" quality={100}/>
+      <h1 className="text-primary">Hello Bezi</h1>
       <Link href="/">Dashboard</Link>
       <Link href="/tickets">Tickets</Link>
     </nav>
--- app/tickets/page.jsx
+++ app/tickets/page.jsx
@@ -1,3 +1,27 @@
+import { Suspense } from "react";
+import { TicketsList } from "./TicketList";
+import Link from "next/link";
+import Loading from "../loading";
+
 export default function Tickets() {
-  return <div>Tickets</div>;
+  return (
+    <main>
+      <nav>
+        <div>
+          <h2>Tickets</h2>
+          <p>
+            <small>Currently open tickets.</small>
+          </p>
+        </div>
+      </nav>
+      <div>
+        <Link href="/tickets/create" className="btn-primary py-1 px-2 rounded-md">
+          Create New Ticket
+        </Link>
+      </div>
+      <Suspense fallback={<Loading />}>
+        <TicketsList />
+      </Suspense>
+    </main>
+  );
 }
